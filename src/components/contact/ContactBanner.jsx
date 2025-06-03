cc673015f1c8ec585dc50090e638ca0056ec494c
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-06-03 11:16:27 -0600

    feat: update contact banner, user is now redirected to the contact page

--- src/components/contact/ContactBanner.jsx
+++ src/components/contact/ContactBanner.jsx
@@ -1,17 +1,17 @@
-import { FaPhone } from "react-icons/fa";
+import { MdContactSupport } from "react-icons/md";
 
 const ContactBanner = () => (
   <div className="p-5 bg-verdeLogo text-white text-center">
     <div className="flex justify-center mb-3">
-      <FaPhone className="text-2xl" />
+      <MdContactSupport className="text-2xl" />
     </div>
     <h4 className="font-bold text-lg mb-1">¿Necesitas más información?</h4>
     <p className="mb-3">Estamos aquí para ayudarte</p>
     <a 
-      href="tel:+523787825552" 
+      href="/contacto" 
       className="text-xl font-bold block hover:text-amarilloLogo transition"
     >
-      +(52) 378 782 5552
+      Contáctanos
     </a>
   </div>
 );
